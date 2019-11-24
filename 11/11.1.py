9041501f65139cbfceb518a34aeacdc830b5f2b6
VenetianDevil <kami@DESKTOP-9NINFTC.localdomain>
2019-11-24 16:07:19 +0100

    11.1 11.2 - gif

---
+++ 11/11.1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-2 -*-
+from generate import *
+
+print (generate_rand(10))
+print (generate_almost_sorted(10))
+print (generate_reversed_sort(10))
+print (generate_gaussian(20, 2, 3))
+print (generate_repeted(10))

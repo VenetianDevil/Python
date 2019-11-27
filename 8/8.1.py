7e2556b9d71d95f3b7bc7a4a2e9bda1042a93e2d
VenetianDevil <kami@DESKTOP-9NINFTC.localdomain>
2019-11-27 22:18:49 +0100

    8.1 poprawione błędy i warunki

--- 8/8.1.py
+++ 8/8.1.py
@@ -8,10 +8,31 @@
 
 def solve1(a, b, c):
     """Rozwiązywanie równania liniowego a x + b y + c = 0."""
-    x = -c / a
-    y = -x / b
-    OX = (x, 0)
-    OY = (0, y)
-    print "Pozwiązanie to prosta przechodzaca przez " + str(OX) + " i " + str(OY)
+    print (a, "x + ", b, "y + ", c, " = 0", "\tRozwiazanie to:")
+    if a == 0 and b==0 and c!=0:
+        raise ValueError("0 != ", c)
+    if a == 0 and b != 0:
+        y = -c / b
+        print ("Zbior punktow, gdzie y = ", y)
+    elif a != 0 and b == 0:
+        x = -c /a
+        print ("Zbior punktow, gdzie x = ", x)
+    elif a != 0 and b != 0:
+        if c == 0 and a == b:
+            print ("Dwusieczna przechodzaca przez I i III cwiartke ukladu, x=y")
+        elif c == 0:
+            print ("Punkt (0,0)")
+        else:
+            x = -c / a
+            y = -c / b
+            OX = (x, 0)
+            OY = (0, y)
+            print ("Prosta przechodzaca przez", OX, " i ", OY)
     
 solve1(1, 2, 3)
+solve1(0, 2, 3)
+solve1(1, 0, 3)
+solve1(1, 0, 0)
+solve1(1, 1, 0)
+solve1(1, 2, 0)
+solve1(0, 0, 1)

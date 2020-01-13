926109ff28822780dbc2efe46bf1019f8c418385
VenetianDevil <kami@DESKTOP-9NINFTC.localdomain>
2020-01-13 17:32:24 +0100

    12 100%

--- 12/12.1.py
+++ 12/12.1.py
@@ -2,6 +2,14 @@
 # -*- coding: iso-8859-2 -*-
 from random import random
 
+def full_search(L, i, n, y):
+    i = i-1
+    while i != None and i < n-1:
+        index = linear_search(L, i+1, n-1, y)
+        if (not (i>0 and index == None)):
+            print (index)
+        i = index
+
 def linear_search(L, left, right, y):
     """Wyszukiwanie liniowe w ciągu."""
     i = left
@@ -23,8 +31,6 @@ k=10
 i=0
 y = int(random()*k)
 L = create_List(n, k)
-print (L)
-print (y)
-while i != None and i < n-1:
-    i = linear_search(L, i+1, n-1, y)
-    print (i)
+# print (L)
+print ("Searching for", y)
+full_search(L, i, n, y)
